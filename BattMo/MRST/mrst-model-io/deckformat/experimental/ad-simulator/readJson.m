db45774cafd0f7b03883f1ce43165d47f1c8f3a6
Ingvild Lyngved Ødegård <ingvlod@stud.ntnu.no>
2022-11-23 18:33:36 +0100

    Cleaned repo

--- BattMo/MRST/mrst-model-io/deckformat/experimental/ad-simulator/readJson.m
+++
@@ -1,9 +0,0 @@
-function json = readJSon(fname)
-fid = fopen(fname);
-raw = fread(fid,inf);
-str = char(raw');
-fclose(fid);
-%%
-json = jsondecode(str);
-end
-
--- BattMo/MRST/mrst-model-io/deckformat/experimental/ad-simulator/writeJson.m
+++
@@ -1,6 +0,0 @@
-function writeJson(sopt,optfile)
-fid = fopen(optfile,'wt');
-fprintf(fid, '%s', jsonencode(sopt));
-fclose(fid); 
-end
-
